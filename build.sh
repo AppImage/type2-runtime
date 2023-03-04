bcc31436fefc4ea4c8b69667d4e5869d3d14beda
probonopd <probonopd@users.noreply.github.com>
2023-03-04 13:44:18 +0100

    zstd-static was folded into zstd-dev
    https://github.com/AppImage/type2-runtime/pull/5#issuecomment-1445173266

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ apk update
 apk add alpine-sdk util-linux strace file autoconf automake libtool
 
 # Build static squashfuse
-apk add fuse3-dev fuse3-static zstd-dev zstd-static zlib-dev zlib-static # fuse-static fuse-dev
+apk add fuse3-dev fuse3-static zstd-dev zlib-dev zlib-static # fuse-static fuse-dev
 wget -c -q "https://github.com/vasi/squashfuse/archive/e51978c.tar.gz"
 tar xf e51978c.tar.gz
 cd squashfuse-*/
