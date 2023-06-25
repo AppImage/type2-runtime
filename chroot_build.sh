642b9458230e0814a0b986d2934cdaaa99f1d4b1
probonopd <probonopd@users.noreply.github.com>
2023-06-25 19:11:51 +0200

    Satisfy shellcheck in chroot_build.sh
    [ci skip]

--- chroot_build.sh
+++ chroot_build.sh
@@ -6,11 +6,11 @@ set -ex
 # Download and extract minimal Alpine system
 #############################################
 
-wget http://dl-cdn.alpinelinux.org/alpine/v3.17/releases/$ARCHITECTURE/alpine-minirootfs-3.17.2-$ARCHITECTURE.tar.gz
+wget "http://dl-cdn.alpinelinux.org/alpine/v3.17/releases/${ARCHITECTURE}/alpine-minirootfs-3.17.2-${ARCHITECTURE}.tar.gz"
 sudo rm -rf ./miniroot  true # Clean up from previous runs
 mkdir -p ./miniroot
 cd ./miniroot
-sudo tar xf ../alpine-minirootfs-*-$ARCHITECTURE.tar.gz
+sudo tar xf ../alpine-minirootfs-*-"${ARCHITECTURE}".tar.gz
 cd -
 
 #############################################
@@ -33,13 +33,11 @@ if [ "$ARCHITECTURE" = "x86" ] || [ "$ARCHITECTURE" = "x86_64" ]; then
     sudo cp build.sh miniroot/build.sh && sudo chroot miniroot /bin/sh -ex /build.sh
 elif [ "$ARCHITECTURE" = "aarch64" ] ; then
     echo "Architecture is aarch64, hence using qemu-aarch64-static"
-    sudo apt-get -y install qemu-user-static
-    sudo cp $(which qemu-aarch64-static) miniroot/usr/bin
+    sudo cp "$(which qemu-aarch64-static)" miniroot/usr/bin
     sudo cp build.sh miniroot/build.sh && sudo chroot miniroot qemu-aarch64-static /bin/sh -ex /build.sh
 elif [ "$ARCHITECTURE" = "armhf" ] ; then
     echo "Architecture is armhf, hence using qemu-arm-static"
-    sudo apt-get -y install qemu-user-static
-    sudo cp $(which qemu-arm-static) miniroot/usr/bin
+    sudo cp "$(which qemu-arm-static)" miniroot/usr/bin
     sudo cp build.sh miniroot/build.sh && sudo chroot miniroot qemu-arm-static /bin/sh -ex /build.sh
 else
     echo "Edit chroot_build.sh to support this architecture as well, it should be easy"
@@ -60,5 +58,5 @@ sudo umount miniroot/proc miniroot/sys miniroot/dev
 if [ "$ARCHITECTURE" = "x86" ] ; then ARCHITECTURE=i686 ; fi
 
 mkdir out/
-sudo find miniroot/ -type f -executable -name 'runtime-fuse3' -exec cp {} out/runtime-$ARCHITECTURE \;
+sudo find miniroot/ -type f -executable -name 'runtime-fuse3' -exec cp {} "out/runtime-${ARCHITECTURE}" \;
 sudo rm -rf miniroot/
