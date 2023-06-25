3eedafe2474d4e07b85f1636bfd7c0aff55d6650
probonopd <probonopd@users.noreply.github.com>
2023-06-25 13:34:04 +0200

    Use qemu-aarch64-static

--- chroot_build.sh
+++ chroot_build.sh
@@ -31,12 +31,19 @@ sudo cp -p /etc/resolv.conf miniroot/etc/
 if [ "$ARCHITECTURE" = "x86" ] || [ "$ARCHITECTURE" = "x86_64" ]; then
     echo "Architecture is x86 or x86_64, hence not using qemu-arm-static"
     sudo cp build.sh miniroot/build.sh && sudo chroot miniroot /bin/sh -ex /build.sh
-else
-    echo "Architecture is something else, hence using qemu-arm-static"
+elif [ "$ARCHITECTURE" = "aarch64" ] ; then
+    echo "Architecture is aarch64, hence using qemu-aarch64-static"
+    sudo apt-get -y install qemu-user-static
+    sudo cp $(which qemu-aarch64-static) miniroot/usr/bin
+    sudo cp build.sh miniroot/build.sh && sudo chroot miniroot qemu-aarch64-static /bin/sh -ex /build.sh
+elif [ "$ARCHITECTURE" = "armv7" ] ; then
+    echo "Architecture is armv7, hence using qemu-arm-static"
     sudo apt-get -y install qemu-user-static
     sudo cp $(which qemu-arm-static) miniroot/usr/bin
-    # sudo chroot miniroot qemu-arm-static /bin/sh -ex <build.sh
     sudo cp build.sh miniroot/build.sh && sudo chroot miniroot qemu-arm-static /bin/sh -ex /build.sh
+else
+    echo "Edit chroot_build.sh to support this architecture as well, it should be easy"
+    exit 1
 fi
 
 #############################################
