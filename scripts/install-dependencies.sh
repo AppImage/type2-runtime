a65a571a8b9fddd8ef7942412a960920f9ffd195
TheAssassin <theassassin@assassinate-you.net>
2024-07-28 14:55:30 +0200

    Fix chroot-based build setup

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,18 +2,33 @@
 
 set -euo pipefail
 
+# use a temporary directory to download the files
+tmpdir="$(mktemp -d)"
+
+# clean it up whenever the script exits
+cleanup() {
+    echo "Cleaning up $tmpdir"
+    rm -rf "$tmpdir"
+}
+trap cleanup EXIT
+
+# store current file's path
+# needed to determine the path of the patches
+this_dir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+
+cd "$tmpdir"
+
 wget https://github.com/libfuse/libfuse/releases/download/fuse-3.15.0/fuse-3.15.0.tar.xz
 echo "70589cfd5e1cff7ccd6ac91c86c01be340b227285c5e200baa284e401eea2ca0  fuse-3.15.0.tar.xz" | sha256sum -c -
 tar xf fuse-3.*.tar.xz
 pushd fuse-3*/
-patch -p1 < /tmp/patches/libfuse/mount.c.diff
+patch -p1 < "$this_dir"/../patches/libfuse/mount.c.diff
 mkdir build
 cd build
 meson setup --prefix=/usr ..
 meson configure --default-library static
 ninja -v install
 popd
-rm -r fuse-*
 
 # Minimize binary size
 export CFLAGS="-ffunction-sections -fdata-sections -Os"
@@ -28,4 +43,3 @@ make -j"$(nproc)"
 make install
 /usr/bin/install -c -m 644 ./*.h '/usr/local/include/squashfuse'
 popd
-rm -r e51978c* squashfuse*
