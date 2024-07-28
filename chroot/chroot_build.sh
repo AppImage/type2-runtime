3de93ce8e2ccd585d3933503fef0a57e976815d4
probonopd <probonopd@users.noreply.github.com>
2024-07-28 18:07:09 +0200

    Add optional build-in-chroot step
    For testing building in chroot; don't have to run this all the time later

--- chroot/chroot_build.sh
+++ chroot/chroot_build.sh
@@ -2,8 +2,9 @@
 
 set -ex
 
-if [ -z "${ARCHITECTURE}" ]; then
-    echo "Usage: env ARCHITECTURE=<arch> $0"
+if [ -z "${ALPINE_ARCH}" ]; then
+    echo "Usage: env ALPINE_ARCH=<arch> $0"
+    echo "Example values: x86_64 x86 armhf aarch64"
     exit 2
 fi
 
@@ -28,10 +29,10 @@ cd "$tempdir"
 # Download and extract minimal Alpine system
 #############################################
 
-wget "http://dl-cdn.alpinelinux.org/alpine/v3.17/releases/${ARCHITECTURE}/alpine-minirootfs-3.17.2-${ARCHITECTURE}.tar.gz"
+wget "http://dl-cdn.alpinelinux.org/alpine/v3.17/releases/${ALPINE_ARCH}/alpine-minirootfs-3.17.2-${ALPINE_ARCH}.tar.gz"
 mkdir -p ./miniroot
 cd ./miniroot
-sudo tar xf ../alpine-minirootfs-*-"${ARCHITECTURE}".tar.gz
+sudo tar xf ../alpine-minirootfs-*-"${ALPINE_ARCH}".tar.gz
 cd -
 
 #############################################
@@ -50,14 +51,14 @@ sudo cp -p /etc/resolv.conf miniroot/etc/
 # Run build.sh in chroot
 #############################################
 
-if [ "$ARCHITECTURE" = "x86" ] || [ "$ARCHITECTURE" = "x86_64" ]; then
+if [ "$ALPINE_ARCH" = "x86" ] || [ "$ALPINE_ARCH" = "x86_64" ]; then
     echo "Architecture is x86 or x86_64, hence not using qemu-arm-static"
     sudo cp "$repo_root_dir"/chroot/build.sh miniroot/build.sh && sudo chroot miniroot /bin/sh -ex /build.sh
-elif [ "$ARCHITECTURE" = "aarch64" ] ; then
+elif [ "$ALPINE_ARCH" = "aarch64" ] ; then
     echo "Architecture is aarch64, hence using qemu-aarch64-static"
     sudo cp "$(which qemu-aarch64-static)" miniroot/usr/bin
     sudo cp "$repo_root_dir"/chroot/build.sh miniroot/build.sh && sudo chroot miniroot qemu-aarch64-static /bin/sh -ex /build.sh
-elif [ "$ARCHITECTURE" = "armhf" ] ; then
+elif [ "$ALPINE_ARCH" = "armhf" ] ; then
     echo "Architecture is armhf, hence using qemu-arm-static"
     sudo cp "$(which qemu-arm-static)" miniroot/usr/bin
     sudo cp "$repo_root_dir"/chroot/build.sh miniroot/build.sh && sudo chroot miniroot qemu-arm-static /bin/sh -ex /build.sh
@@ -71,8 +72,15 @@ fi
 #############################################
 
 # Use the same architecture names as https://github.com/AppImage/AppImageKit/releases/
-if [ "$ARCHITECTURE" = "x86" ] ; then ARCHITECTURE=i686 ; fi
+case "$ALPINE_ARCH" in
+    x86)
+        appimage_arch=i686
+        ;;
+    *)
+        appimage_arch="$ALPINE_ARCH"
+        ;;
+esac
 
 cd "$repo_root_dir"
 mkdir -p ./out/
-sudo find "$tempdir"/miniroot/ -type f -executable -name 'runtime-fuse3' -exec cp {} "out/runtime-${ARCHITECTURE}" \;
+sudo find "$tempdir"/miniroot/ -type f -executable -name 'runtime-fuse3' -exec cp {} "out/runtime-${appimage_arch}" \;
