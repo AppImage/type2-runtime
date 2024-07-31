AppImage/type2-runtime

commit cc87adefa2ea5972c40d9f94f2f2578187096ccc

Add -Wall -Werror (#43)
* Add -Wall -Werror
* Reduce related compiler warnings

Closes #42

Thanks @TheAssassin and @Korne127

src/runtime/runtime.c
@@ -179,7 +179,6 @@ typedef struct elf32_note {
 
 typedef Elf32_Nhdr Elf_Nhdr;
 
-static char* fname;
 static Elf64_Ehdr ehdr;
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
@@ -218,8 +217,7 @@ static off_t read_elf32(FILE* fd) {
     fseeko(fd, 0, SEEK_SET);
     ret = fread(&ehdr32, 1, sizeof(ehdr32), fd);
     if (ret < 0 || (size_t) ret != sizeof(ehdr32)) {
-        fprintf(stderr, "Read of ELF header from %s failed: %s\n",
-                fname, strerror(errno));
+        fprintf(stderr, "Read of ELF header failed: %s\n", strerror(errno));
         return -1;
     }
 
@@ -231,8 +229,7 @@ static off_t read_elf32(FILE* fd) {
     fseeko(fd, last_shdr_offset, SEEK_SET);
     ret = fread(&shdr32, 1, sizeof(shdr32), fd);
     if (ret < 0 || (size_t) ret != sizeof(shdr32)) {
-        fprintf(stderr, "Read of ELF section header from %s failed: %s\n",
-                fname, strerror(errno));
+        fprintf(stderr, "Read of ELF section header failed: %s\n", strerror(errno));
         return -1;
     }
 
@@ -252,8 +249,7 @@ static off_t read_elf64(FILE* fd) {
     fseeko(fd, 0, SEEK_SET);
     ret = fread(&ehdr64, 1, sizeof(ehdr64), fd);
     if (ret < 0 || (size_t) ret != sizeof(ehdr64)) {
-        fprintf(stderr, "Read of ELF header from %s failed: %s\n",
-                fname, strerror(errno));
+        fprintf(stderr, "Read of ELF header failed: %s\n", strerror(errno));
         return -1;
     }
 
@@ -265,8 +261,7 @@ static off_t read_elf64(FILE* fd) {
     fseeko(fd, last_shdr_offset, SEEK_SET);
     ret = fread(&shdr64, 1, sizeof(shdr64), fd);
     if (ret < 0 || ret != sizeof(shdr64)) {
-        fprintf(stderr, "Read of ELF section header from %s failed: %s\n",
-                fname, strerror(errno));
+        fprintf(stderr, "Read of ELF section header failed: %s\n", strerror(errno));
         return -1;
     }
 
@@ -289,8 +284,7 @@ ssize_t appimage_get_elf_size(const char* fname) {
     }
     ret = fread(ehdr.e_ident, 1, EI_NIDENT, fd);
     if (ret != EI_NIDENT) {
-        fprintf(stderr, "Read of e_ident from %s failed: %s\n",
-                fname, strerror(errno));
+        fprintf(stderr, "Read of e_ident from %s failed: %s\n", fname, strerror(errno));
         return -1;
     }
     if ((ehdr.e_ident[EI_DATA] != ELFDATA2LSB) &&
@@ -904,8 +898,8 @@ int fusefs_main(int argc, char* argv[], void (* mounted)(void)) {
     struct fuse_opt fuse_opts[] = {
             {"offset=%zu", offsetof(sqfs_opts, offset), 0},
             {"timeout=%u", offsetof(sqfs_opts, idle_timeout_secs), 0},
-            {"fsname=%s", "squashfuse", 0},
-            {"subtype=%s", "squashfuse", 0},
+            {"fsname=squashfuse", 0},
+            {"subtype=squashfuse", 0},
             FUSE_OPT_END
     };
 
@@ -923,7 +917,7 @@ int fusefs_main(int argc, char* argv[], void (* mounted)(void)) {
     sqfs_ll_ops.readlink = sqfs_ll_op_readlink;
     sqfs_ll_ops.listxattr = sqfs_ll_op_listxattr;
     sqfs_ll_ops.getxattr = sqfs_ll_op_getxattr;
-    sqfs_ll_ops.forget = sqfs_ll_op_forget;
+    sqfs_ll_ops.forget = (void (*)(struct fuse_req *, fuse_ino_t, uint64_t))sqfs_ll_op_forget;
     sqfs_ll_ops.statfs = stfs_ll_op_statfs;
 
     /* PARSE ARGS */
@@ -1422,7 +1416,6 @@ int main(int argc, char* argv[]) {
     }
 
     // calculate full path of AppImage
-    int length;
     char fullpath[PATH_MAX];
 
     if (getenv("TARGET_APPIMAGE") == NULL) {
@@ -1459,14 +1452,15 @@ int main(int argc, char* argv[]) {
             Md5Initialise(&ctx);
 
             char buf[4096];
-            for (size_t bytes_read; (bytes_read = fread(buf, sizeof(char), sizeof(buf), f)); bytes_read > 0) {
+            size_t bytes_read;
+            while ((bytes_read = fread(buf, sizeof(char), sizeof(buf), f)) > 0) {
                 Md5Update(&ctx, buf, (uint32_t) bytes_read);
             }
 
             MD5_HASH digest;
             Md5Finalise(&ctx, &digest);
 
-            hexlified_digest = appimage_hexlify(digest.bytes, sizeof(digest.bytes));
+            hexlified_digest = appimage_hexlify((const char*)digest.bytes, sizeof(digest.bytes));
         }
 
         char* prefix = malloc(strlen(temp_base) + 20 + strlen(hexlified_digest) + 2);

src/runtime/Makefile
@@ -1,5 +1,5 @@
 CC            = gcc
-CFLAGS        = -std=gnu99 -s -Os -D_FILE_OFFSET_BITS=64 -DGIT_COMMIT=\"${GIT_COMMIT}\" -T data_sections.ld -ffunction-sections -fdata-sections -Wl,--gc-sections -static
+CFLAGS        = -std=gnu99 -s -Os -D_FILE_OFFSET_BITS=64 -DGIT_COMMIT=\"${GIT_COMMIT}\" -T data_sections.ld -ffunction-sections -fdata-sections -Wl,--gc-sections -static -Wall -Werror
 LIBS          = -lsquashfuse -lsquashfuse_ll -lzstd -lz
 
 all: runtime-fuse3 runtime-fuse3

commit 4d92e9fbbe7c8ea6d8ae7ca16aea4b164f91c7d8

Fix use of basename
Fixes two related compiler errors introduced by #43.

src/runtime/runtime.c
@@ -61,6 +61,7 @@ extern int sqfs_opt_proc(void* data, const char* arg, int key, struct fuse_args*
 #include <fnmatch.h>
 #include <sys/mman.h>
 #include <stdint.h>
+#include <libgen.h>
 
 typedef struct {
     uint32_t lo;
@@ -864,8 +865,10 @@ bool rm_recursive(const char* const path) {
 void build_mount_point(char* mount_dir, const char* const argv0, char const* const temp_base, const size_t templen) {
     const size_t maxnamelen = 6;
 
-    char* path_basename;
-    path_basename = basename(argv0);
+    // need to copy argv0 as it's a const value, basename intends to modify it
+    char* argv0_copy = strdup(argv0);
+    char* path_basename = basename(argv0_copy);
+    free(argv0_copy);
 
     size_t namelen = strlen(path_basename);
     // limit length of tempdir name