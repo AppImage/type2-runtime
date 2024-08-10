0d74e0c26c3f1b96507be922b0b536258b397b46
probonopd <probonopd@users.noreply.github.com>
2024-08-10 07:52:40 +0200

    Fix #66

--- src/runtime/runtime.c
+++ src/runtime/runtime.c
@@ -662,7 +662,8 @@ void print_help(const char* appimage_path) {
     fprintf(stderr,
             "AppImage options:\n\n"
             "  --appimage-extract [<pattern>]  Extract content from embedded filesystem image\n"
-            "                                  If pattern is passed, only extract matching files\n"
+            "                                  If pattern is passed, only extract matching\n"
+            "                                  files\n"
             "  --appimage-extract-and-run      Temporarily extract content from embedded\n"
             "                                  filesystem image, run contained application,\n"
             "                                  then delete temporarily extracted content\n"
